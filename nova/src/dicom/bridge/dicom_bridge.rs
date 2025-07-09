7f28e4d140c8bf49f496b6e28f2116c60e033030
jaogren <jacob.ogren2@gmail.com>
2025-07-09 23:28:00 +0200

    use some features in latest rust as of writing (1.88.0)

--- nova/src/dicom/bridge/dicom_bridge.rs
+++ nova/src/dicom/bridge/dicom_bridge.rs
@@ -1,3 +1,7 @@
+use std::ffi::{c_char, CStr};
+use parking_lot::Once;
+use tracing::{debug, error, info};
+use tracing::log::warn;
 
 #[cxx::bridge]
 pub mod dicom_api {
@@ -23,40 +27,38 @@ pub mod dicom_api {
     }
 }
 
-use std::ffi::{c_char, CStr};
-use tracing::{debug, error, info};
-use tracing::log::warn;
-
 fn log_thunk(level: &str, msg: &str) {
     const TARGET: &str = "nova::cxx";
 
     match level {
         "debug" => debug!(target: TARGET, "{}", msg),
         "info"  => info!(target: TARGET, "{}", msg),
-        "warn" | "warning"  => warn!(target: TARGET, "{}", msg),
+        "warn" | "warning"  => warn!(target: TARGET, "{msg}"),
         "error" | "fatal" | "critical" => error!(target: TARGET, "{}", msg),
-        unknown_lvl => warn!(
-            target: TARGET,
-            "Unknown log level '{}', message: {}", unknown_lvl, msg
-        ),
+        unknown_lvl => warn!(target: TARGET,"Unknown log level '{unknown_lvl}', message: {unknown_lvl}"),
     }
 }
 
-extern "C" {
+unsafe extern "C" {
     fn set_log_callback(cb: extern "C" fn(level: *const c_char, msg: *const c_char));
 }
 
 extern "C" fn cxx_log_callback(level: *const c_char, msg: *const c_char) {
     unsafe {
-        let msg = CStr::from_ptr(msg).to_string_lossy();
-        let level = CStr::from_ptr(level).to_string_lossy();
-
-        log_thunk(&level, &msg);
+        if let Ok(level) = CStr::from_ptr(level).to_str()
+            && let Ok(msg) = CStr::from_ptr(msg).to_str()
+        {
+            log_thunk(level, msg);
+        }
+        else {
+            warn!(target: "nova::cxx", "Invalid UTF-8 from native logger");
+        }
     }
 }
 
 pub fn register_logger_service() {
-    unsafe {
+    static INIT: Once = Once::new();
+    INIT.call_once(|| unsafe {
         set_log_callback(cxx_log_callback);
-    }
+    });
 }
