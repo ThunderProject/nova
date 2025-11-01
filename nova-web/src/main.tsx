2b99dcc3415b7374a71b3d0205d9dea6d230c6d6
jaogren <jacob.ogren2@gmail.com>
2025-11-01 23:06:07 +0100

    update main

--- nova-web/src/main.tsx
+++ nova-web/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {BrowserRouter} from "react-router";
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.querySelector('#root')!).render(
     <StrictMode>
         <BrowserRouter>
             <App/>
