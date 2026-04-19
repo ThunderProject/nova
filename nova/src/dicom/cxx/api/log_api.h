36e1fb51de2ad27797bb819adb46a8b09f70431b
jaogren <jaogren@jaogren.localdomain>
2026-04-19 14:06:27 +0200

    remove stuff

--- nova/src/dicom/cxx/api/log_api.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace nova::api {
-    using logFunc = void(*)(const char* level, const char* message);
-
-    extern "C" __declspec(dllexport) void set_log_callback(logFunc cb);
-    void send_log(const std::string& level, const std::string& msg);
-}
