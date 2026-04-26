6fdea513dd28d9605348625f7d07e9de88044d2b
jaogren <jaogren@jaogren.localdomain>
2026-04-26 17:27:42 +0200

    removed ui, to be replaced by avalonia

--- nova-web/src/utils/PasswordGenerator.ts
+++
@@ -1,9 +0,0 @@
-export class PasswordGenerator {
-    public static generate(length: number = 18): string {
-        const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}<>?';
-        const randomValues = new Uint32Array(length);
-
-        crypto.getRandomValues(randomValues);
-        return Array.from(randomValues, (val) => charset[val % charset.length]).join('');
-    }
-}
